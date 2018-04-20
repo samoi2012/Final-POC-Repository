efb3c556e4fd2c9b667c29b9e294bbfb40159c66
Model Repository <ModelRepository@gmail.com>
2018-04-20 17:06:51 +0530

    Final POC Commit

--- POC/src/test/java/HTC/POC/SampleTestCase.java
+++ POC/src/test/java/HTC/POC/SampleTestCase.java
@@ -22,7 +22,7 @@ public class SampleTestCase {
 	  browser.manage().window().maximize();
 	  browser.findElement(By.xpath("//a[@class='emp-login-btn']")).click();
 	  browser.findElement(By.name("user_name")).sendKeys("20979");
-	  browser.findElement(By.name("user_pass")).sendKeys("krishna@1994");
+	  browser.findElement(By.name("user_pass")).sendKeys("Krishna@1994");
 	  browser.findElement(By.xpath("//button[@class='btn btn-lg btn-primary btn-block btn-default']")).click();
 	  browser.findElement(By.xpath("//span[@class='logout_text']")).click();
 	  browser.quit();
