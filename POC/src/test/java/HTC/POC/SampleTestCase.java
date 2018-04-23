870b288c091b10c70ba557f6da8bcf4b5a6d1de5
Model Repository <ModelRepository@gmail.com>
2018-04-23 19:00:55 +0530

    local repository commit

--- POC/src/test/java/HTC/POC/SampleTestCase.java
+++ POC/src/test/java/HTC/POC/SampleTestCase.java
@@ -28,8 +28,7 @@ public class SampleTestCase {
 	  browser.quit();
 	  
 	  System.out.println("Test Completed");
-	  System.out.println("Modified for testing the automation build creation.trial number 3");
-	  System.out.println("Modified for testing the automation build creationfrom local repository");
+	  System.out.println("Modified for testing the automation build creationfrom local repository trial number 2");
 	  
 	  
 	  
